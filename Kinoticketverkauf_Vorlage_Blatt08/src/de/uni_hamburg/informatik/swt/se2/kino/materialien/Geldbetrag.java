1d673e2d885ef86b93dec41d8d51ca6d072b7f10
4neves <4neves@rzpc156.informatik.uni-hamburg.de>
2015-06-22 15:48:41 +0200

    easy schnittstellenkommentare

--- Kinoticketverkauf_Vorlage_Blatt08/src/de/uni_hamburg/informatik/swt/se2/kino/materialien/Geldbetrag.java
+++ Kinoticketverkauf_Vorlage_Blatt08/src/de/uni_hamburg/informatik/swt/se2/kino/materialien/Geldbetrag.java
@@ -17,6 +17,11 @@ public class Geldbetrag {
 		parseEuroCent(eurocent);
 	}
 	
+	/**
+	 * Verarbeitet einen eingegebenen Geldbetrag als String und gibt ihn als Integer in Cent aus.
+	 * @param eurocent Geldbetrag in Cent
+	 * @return
+	 */
     private int parseString(String eurocent)
     {
     	//Pattern p = Pattern.compile("[0-9]++,{0,1}[0-9]{0,2}");
@@ -38,32 +43,59 @@ public class Geldbetrag {
     	return euro*100 + cent;
     }
     
+    /**
+     * Verarbeitet einen Geldbetrag als int und belegt die Variablen _cent und _euro mit dem korrekten Wert.
+     * @param eurocent Der Betrag in Cent
+     */
     private void parseEuroCent(int eurocent)
     {
 		_cent = eurocent%100;
 		_euro = eurocent/100;
     }
     
+    /**
+     * Zieht einen übergebenen Betrag vom Geldbetrag ab.
+     * @param eurocent Abzuziehender Betrag in Cent
+     * @return Geldbetrag
+     */
     public Geldbetrag subtract(int eurocent) 
     {
 		return new Geldbetrag(getEuroCentInt() - eurocent);
 	}
     
+    /**
+     * Zieht einen als String übergebenen Betrag vom Geldbetrag ab.
+     * @param eurocent Betrag in Cent
+     * @return Geldbetrag
+     */
     public Geldbetrag subtract(String eurocent) 
     {
 		return new Geldbetrag(getEuroCentInt() - parseString(eurocent));
 	}
     
+    /**
+     * Zieht einen als Geldbetrag übergebenen Betrag vom Geldbetrag ab.
+     * @param eurocent Betrag in Cent
+     * @return Geldbetrag
+     */
     public Geldbetrag subtract(Geldbetrag geldbetrag) 
     {
 		return new Geldbetrag(getEuroCentInt() - geldbetrag.getEuroCentInt());
 	}
     
+    /**
+     * Gibt den Geldbetrag in Cent als int aus.
+     * @return
+     */
     public int getEuroCentInt() 
     {
     	return _euro*100 + _cent;
     }
     
+    /**
+     * Gibt den Geldbetrag in Cent als String aus.
+     * @return
+     */
     public String getEuroCentString() 
     {
     	//return "" + (_cent < 0 ? "-" : "" ) + _euro + "," + (_cent < 10 ? (_cent < 0 ? (_cent < -9 ? _cent*-1 : "0" + _cent*-1) : "0" + _cent) : _cent);
